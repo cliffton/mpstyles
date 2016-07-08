fc6eb7dab9c3180f08e0b5176f92a4655b42a3ac
Neville Dsouza <nevilledsouza28@gmail.com>
2016-07-09 01:39:16 +0530

    completed login service

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ManPriya Styles !
+Open CMD and go the path in which the downloaded/cloned repository exist.
+
+enter command -- npm install
+enter command -- npm start
+
+copy the url shown on cmd to any web browser after the commands are executed and append index.html.
+eg: http://localhost:8000/index.html#/home
--- static/app/login/login.js
+++ static/app/login/login.js
@@ -1,7 +1,30 @@
-'use strict';
+angular.module('myApp.login', ['myApp.services.authentication'])
 
-angular.module('myApp.login', [])
+.controller('loginCtrl', function($scope, $rootScope, $location, AuthenticationService) {
 
-.controller('loginCtrl', [function() {
+$scope.notMemberNote = "Not a Member ? Register with us";
+$scope.forgotPasswordNote = "forgot password ?";
+$scope.rememberNote = "remember me";
+$scope.home = "HOME";
+$scope.dataLoading = false;
+$scope.isInvalid = false;
+$scope.errorMessage = "";
 
-}]);
+
+// reset login status
+AuthenticationService.clearCredentials();
+
+$scope.login = function () {
+    $scope.dataLoading = true;
+    AuthenticationService.login($scope.email, $scope.password, function(response) {
+        if(response.success) {
+            AuthenticationService.setCredentials($scope.email, $scope.password);
+            $location.path('/');
+        } else {
+            $scope.errorMessage = response.message;
+            $scope.dataLoading = false;
+            $scope.isInvalid = true;
+        }
+    });
+};
+});
