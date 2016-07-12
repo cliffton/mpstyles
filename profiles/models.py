8430af514b63409bb6819c44bbd6c9ba27ac03c7
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-07-13 01:20:42 +0530

    - profiles app added.

---
+++ profiles/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+from django.contrib.auth.models import AbstractUser
+from rest_framework.authtoken.models import Token
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    contact_number = models.CharField(max_length=13, unique=True, default=None, null=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    dob = models.DateField(blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
