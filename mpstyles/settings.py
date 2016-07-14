21dc26d51afecbc64e4a71ae8b621d20003b2e29
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-07-15 01:28:31 +0530

    - static root added.

--- mpstyles/settings.py
+++ mpstyles/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ubuntu/mpstyles/static/'
 
 AUTH_USER_MODEL = 'profiles.User'
