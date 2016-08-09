f971dd268bc42f0c14e3ffe852c0427899f34e6a
Neville Dsouza <nevilledsouza28@gmail.com>
2016-08-10 01:00:16 +0530

    added product description page

--- static/app/productDescription/productDescription.js
+++ static/app/productDescription/productDescription.js
@@ -2,6 +2,19 @@
 
 angular.module('myApp.productDescription', [])
 
-.controller('productDescriptionCtrl', function() {
-	
+.controller('productDescriptionCtrl', function($scope) {
+
+$scope.home = "Home";
+
+$scope.productDetails = {
+    	    id: 1,
+    	    name: "Beeded chain",
+    	    productCode: "ABC123",
+    	    price: 250,
+    	    sourcePath: "content/img/image5.jpg",
+    	    isLimited: false,
+    	    stockQuantity: 25,
+    	    description: ""
+};
+
 });
