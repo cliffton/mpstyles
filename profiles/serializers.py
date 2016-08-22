5d113134646e265f269b8d90f74d3bbb8ec0cfeb
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-08-23 01:52:11 +0530

    - Unique validators added in serializer.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from profiles.models import User
 
 
 class RegistrationSerializer(serializers.Serializer):
-	first_name = serializers.CharField(required=True)
-	last_name = serializers.CharField(required=True)
-	contact_number = serializers.CharField(required=True)
-	email = serializers.EmailField(required=True)
-	# dob  = serializers.CharField(required=True)
-	password = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    contact_number = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    # dob  = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
