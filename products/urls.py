854fc0967e96d5ad51868911bc104406d077dc0d
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-08-26 00:58:00 +0530

    - ProductDetail and Image urls added.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
-from products.views import ProductViewSet
+from products.views import ProductViewSet, ProductDetailViewSet, ImageViewSet
 
 router = routers.SimpleRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'product-details', ProductDetailViewSet)
+router.register(r'images', ImageViewSet)
 
 urlpatterns = router.urls
