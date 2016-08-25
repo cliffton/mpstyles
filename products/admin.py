dbedcf8696bdc231f20b7c2d727de726d8687390
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-08-26 00:57:16 +0530

    - New models registered in admin

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from products.models import Category, SubCategory, Product
+from products.models import (
+    Category, SubCategory, Product, ProductDetail, Image)
 
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Product)
+admin.site.register(ProductDetail)
+admin.site.register(Image)
