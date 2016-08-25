e16fc33622d57802eb2f1a51c6d882ea2d2e2f61
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-08-26 00:57:48 +0530

    - ProductDetail and Image viewsets added.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from products.models import Product
-from products.serializers import ProductSerializer
+from products.models import Product, ProductDetail, Image
+from products.serializers import (
+    ProductSerializer, ProductDetailSerializer, ImageSerializer)
 # Create your views here.
 
 
@@ -9,3 +10,15 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+
+class ProductDetailViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ProductDetailSerializer
+    queryset = ProductDetail.objects.all()
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ImageSerializer
+    queryset = Image.objects.all()
