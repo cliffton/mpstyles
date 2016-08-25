0c603f3a29ffce62697311a856406b5dd252ee2a
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-08-26 00:57:40 +0530

    - ProductDetail and Image Serializers added

--- products/serializers.py
+++ products/serializers.py
@@ -1,20 +1,40 @@
 from rest_framework import serializers
-from products.models import Category, SubCategory, Product
+from products.models import (
+    Category, SubCategory, Product, ProductDetail, Image)
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        exclude = ('uuid', 'created', 'modified')
+        exclude = ('uuid', 'created', 'modified', 'id')
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = SubCategory
-        exclude = ('uuid', 'created', 'modified')
+        exclude = ('uuid', 'created', 'modified', 'id')
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        exclude = ('uuid', 'created', 'modified', 'product_detail', 'id')
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ProductDetail
+        exclude = ('uuid', 'created', 'modified', 'id', 'product')
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_details = ProductDetailSerializer(read_only=True)
+    subcategory = SubCategorySerializer(read_only=True)
+
     class Meta:
         model = Product
         exclude = ('uuid', 'created', 'modified')
